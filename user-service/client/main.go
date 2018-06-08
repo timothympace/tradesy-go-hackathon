aff656743aa81e5bca901644a86795666c6e6541
Offspring2099 <dmitriy@labitov.com>
2018-06-08 14:23:29 -0700

    Add user-service

---
+++ user-service/client/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"io"
+	"log"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "github.com/timothympace/tradesy-go-hackathon/user-service/user"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+// AddUser calls the RPC method AddUser of UserApiClient
+func addUser(client pb.UserApiClient, user *pb.User) {
+	resp, err := client.AddUser(context.Background(), user)
+	if err != nil {
+		log.Fatalf("Could not create User: %v", err)
+	}
+	if resp.Success {
+		log.Printf("A new User has been added with id: %d", resp.Id)
+	}
+}
+
+// UpdateUser calls the RPC method UpdateUser of UserApiClient
+func updateUser(client pb.UserApiClient, user *pb.User) {
+	resp, err := client.UpdateUser(context.Background(), user)
+	if err != nil {
+		log.Fatalf("Could not update User: %v", err)
+	}
+	if resp.Success {
+		log.Printf("A new User has been updated with id: %d", resp.Id)
+	}
+}
+
+func deleteUser(client pb.UserApiClient, filter *pb.UserFilter) {
+	resp, err := client.DeleteUser(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("Could not delete User: %v", err)
+	}
+	if resp.Success {
+		log.Printf("A new has been deleted with id: %d", resp.Id)
+	}
+}
+
+// getCustomers calls the RPC method GetCustomers of CustomerServer
+func getUser(client pb.UserApiClient, filter *pb.UserFilter) {
+	// calling the streaming API
+	stream, err := client.GetUser(context.Background(), filter)
+	if err != nil {
+		log.Fatalf("Error on get user: %v", err)
+	}
+	for {
+		// Receiving the stream of data
+		user, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("%v.GetUser(_) = _, %v", client, err)
+		}
+		log.Printf("User: %v", user)
+	}
+}
+func main() {
+	// Set up a connection to the gRPC server.
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	// Creates a new CustomerClient
+	client := pb.NewUserApiClient(conn)
+
+	user := &pb.User{
+		Id:    "101",
+		Name:  "Shiju Varghese",
+		Email: "shiju@xyz.com",
+		Phone: "732-757-2923",
+		Addresses: []*pb.User_Address{
+			&pb.User_Address{
+				Street:            "1 Mission Street",
+				City:              "San Francisco",
+				State:             "CA",
+				Zip:               "94105",
+				IsShippingAddress: false,
+			},
+			&pb.User_Address{
+				Street:            "Greenfield",
+				City:              "Kochi",
+				State:             "KL",
+				Zip:               "68356",
+				IsShippingAddress: true,
+			},
+		},
+	}
+
+	// Create a new customer
+	addUser(client, user)
+
+	user = &pb.User{
+		Id:    "102",
+		Name:  "Irene Rose",
+		Email: "irene@xyz.com",
+		Phone: "732-757-2924",
+		Addresses: []*pb.User_Address{
+			&pb.User_Address{
+				Street:            "1 Mission Street",
+				City:              "San Francisco",
+				State:             "CA",
+				Zip:               "94105",
+				IsShippingAddress: true,
+			},
+		},
+	}
+
+	// Create a new user
+	addUser(client, user)
+
+	user = &pb.User{
+		Id:    "100",
+		Name:  "Chuck Berry",
+		Email: "c@b.com",
+		Phone: "818-867-5309",
+		Addresses: []*pb.User_Address{
+			&pb.User_Address{
+				Street:            "2nd Mission Street",
+				City:              "Los Francisco",
+				State:             "NV",
+				Zip:               "88888",
+				IsShippingAddress: false,
+			},
+		},
+	}
+
+	// Create a new user
+	addUser(client, user)
+
+	// Update a user
+	user.Name = "Tim Rose"
+	updateUser(client, user)
+
+	// Delete a user
+	filter := &pb.UserFilter{Id: "101"}
+	deleteUser(client, filter)
+
+	// Filter with an empty Id
+	filter = &pb.UserFilter{Id: ""}
+	getUser(client, filter)
+
+}
---
+++ user-service/server/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"log"
+	"net"
+	"strings"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+
+	pb "github.com/timothympace/tradesy-go-hackathon/user-service/user"
+)
+
+const (
+	port = ":50051"
+)
+
+// server is used to implement user.UserServer.
+type server struct {
+	savedUsers []*pb.User
+}
+
+// AddUser creates a new User
+func (s *server) AddUser(ctx context.Context, in *pb.User) (*pb.UserResponse, error) {
+	s.savedUsers = append(s.savedUsers, in)
+	return &pb.UserResponse{Id: in.Id, Success: true}, nil
+}
+
+// UpdateUser creates a new User
+func (s *server) UpdateUser(ctx context.Context, in *pb.User) (*pb.UserResponse, error) {
+	//s.savedUsers = append(s.savedUsers, in)
+	//return &pb.UserResponse{Id: in.Id, Success: true}, nil
+	for index, user := range s.savedUsers {
+		if in.Id != "" {
+			if strings.Contains(user.Id, in.Id) {
+				s.savedUsers[index] = in
+				return &pb.UserResponse{Id: in.Id, Success: true}, nil
+			}
+		}
+	}
+	return &pb.UserResponse{Id: in.Id, Success: false}, nil
+}
+
+// GetUser returns all users by given filter
+func (s *server) GetUser(filter *pb.UserFilter, stream pb.UserApi_GetUserServer) error {
+	for _, user := range s.savedUsers {
+		if filter.Id != "" {
+			if !strings.Contains(user.Name, filter.Id) {
+				continue
+			}
+		}
+		if err := stream.Send(user); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+// DeleteUser deletes user by given filter
+func (s *server) DeleteUser(ctx context.Context, filter *pb.UserFilter) (*pb.UserResponse, error) {
+	for i, user := range s.savedUsers {
+		if filter.Id != "" {
+			if !strings.Contains(user.Name, filter.Id) {
+				s.savedUsers = append(s.savedUsers[:i], s.savedUsers[i+1:]...)
+				return &pb.UserResponse{Id: filter.Id, Success: true}, nil
+			}
+		}
+	}
+	return &pb.UserResponse{Id: filter.Id, Success: false}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	// Creates a new gRPC server
+	s := grpc.NewServer()
+	pb.RegisterUserApiServer(s, &server{})
+	s.Serve(lis)
+}
